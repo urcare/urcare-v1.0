3d2144dbbee1eaacb801e79331597e8bd995afa5
geekybouy <sarthaks264@gmail.com>
2025-07-25 13:32:59 +0530

    chore: rename OnDemandLanding.tsx to Landing.tsx and update imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useAuth } from './contexts/AuthContext';
-import Landing from './pages/OnDemandLanding';
+import Landing from './pages/Landing';
 import Auth from './pages/Auth';
 import Onboarding from './pages/Onboarding';
 import CustomPlan from './pages/CustomPlan';
